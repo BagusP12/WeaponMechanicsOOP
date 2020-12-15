d7dfc4877c42356faa03de6ff7ee50c288903320
bagusp <bagoezsoediro@gmail.com>
2020-12-15 17:16:20 +0700

    Update shotgun

--- src/com/venom/App.java
+++ src/com/venom/App.java
@@ -12,47 +12,26 @@ public class App {
 
         AssaultRifle m4a1 = new AssaultRifle("M4A1", "Assault Rifle", 30, 30, 120, 1300, 80, 70, 3);
 
+        Shotgun spas12 = new Shotgun("Franchi SPAS-12", "Shotgun", 35, 7, 42, 80, 40, 90, false);
+
         Scope vortexScope = new Scope("Vortex Spitfire 3x Scope", "Scope", 3);
+        Scope ironSight = new Scope("Iron Sight", "Scope", 2);
 
         LaserSight gnpLaser = new LaserSight("G&P T1 Laser Sight", "Laser Sight", 5);
 
         Grip fabGrip = new Grip("FAB Finger Grooves REG", "Fore Grip", 10);
 
-        desertEagle.weaponInfo();
-        desertEagle.shoot();
-        desertEagle.shoot();
-        desertEagle.shoot();
-        desertEagle.reload();
+        spas12.weaponInfo();
 
-        desertEagle.installAttachment(vortexScope);
-        desertEagle.installAttachment(gnpLaser);
-        desertEagle.weaponInfo();
+        spas12.shoot();
+        spas12.shoot();
+        spas12.shoot();
 
-        m4a1.weaponInfo();
-        m4a1.changeFireMode("burst");
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.changeFireMode("single");
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.changeFireMode("auto");
-        m4a1.shoot();
-        m4a1.changeFireMode("single");
-        m4a1.shoot();
+        spas12.reload();
 
-        m4a1.installAttachment(vortexScope);
-        m4a1.installAttachment(fabGrip);
+        spas12.changeFireMode("auto");
 
-        m4a1.weaponInfo();
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.shoot();
-        m4a1.pickupAmmo(53);
+        spas12.shoot();
 
     }
     
